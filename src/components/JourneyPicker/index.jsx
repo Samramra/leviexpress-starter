4c1b9e7f5fe62a6d3f244a29b392ccd0e15cc7e2
Šárka Mráčková <sarka7091@gmail.com>
2022-05-21 11:34:11 +0200

    Údaje o cestě

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -2,7 +2,24 @@ import React, { useEffect, useState } from 'react';
 import mapImage from './img/map.svg';
 import './style.css';
 
-export const JourneyPicker = ({ onJourneyChange }) => (
+
+
+
+
+export const JourneyPicker = ({ onJourneyChange }) => {
+
+  const [fromCity, setFromCity] = useState() 
+  const [toCity, setToCity] = useState() 
+  const [date, setDate] = useState() 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({fromCity});
+    console.log({toCity});
+    console.log({date});
+  };
+  
+    return (
+
   <div className="journey-picker container">
     <h2 className="journey-picker__head">Kam chcete jet?</h2>
     <div className="journey-picker__body">
@@ -11,40 +28,39 @@ export const JourneyPicker = ({ onJourneyChange }) => (
           <div className="journey-picker__label">Odkud:</div>
           <select>
             <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
+            <option value="mesto01" onChange={() => setFromCity(FromCity)}>Město1</option>
+            <option value="mesto02" onChange={() => setFromCity(FromCity)}>Město2</option>
+            <option value="mesto03" onChange={() => setFromCity(FromCity)}>Město3</option>
+            <option value="mesto04" onChange={() => setFromCity(FromCity)}>Město4</option>
+            <option value="mesto05" onChange={() => setFromCity(FromCity)}>Město5</option>
           </select>
         </label>
         <label>
           <div className="journey-picker__label">Kam:</div>
           <select>
             <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
+            <option value="mesto01" onChange={() => setToCity(toCity)}>Město1</option>
+            <option value="mesto02" onChange={() => setToCity(toCity)}>Město2</option>
+            <option value="mesto03" onChange={() => setToCity(toCity)}>Město3</option>
+            <option value="mesto04" onChange={() => setToCity(toCity)}>Město4</option>
+            <option value="mesto05" onChange={() => setToCity(toCity)}>Město5</option>
           </select>
         </label>
         <label>
           <div className="journey-picker__label">Datum:</div>
           <select>
             <option value="">Vyberte</option>
-            <option value="datum01">Datum 01</option>
-            <option value="datum02">Datum 02</option>
-            <option value="datum03">Datum 03</option>
-            <option value="datum04">Datum 04</option>
-            <option value="datum05">Datum 05</option>
+            <option value="datum01" onChange={() => setDate(date)}>datum01</option>
+            <option value="datum02" onChange={() => setDate(date)}>datum02</option>
+            <option value="datum03" onChange={() => setDate(date)}>datum03</option>
+            <option value="datum04" onChange={() => setDate(date)}>datum04</option>
+            <option value="datum05" onChange={() => setDate(date)}>datum05</option>
           </select>
         </label>
         <div className="journey-picker__controls">
           <button 
             className="btn" 
-            type="submit"
-          > 
+            type="submit"  onClick={handleSubmit}>
             Vyhledat spoj
           </button>
         </div>
@@ -52,4 +68,5 @@ export const JourneyPicker = ({ onJourneyChange }) => (
       <img className="journey-picker__map" src={mapImage} />
     </div>
   </div>
-);
+    )
+};
